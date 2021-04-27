5ab05191265a2e5fa28dce255de687c7bbd21e9a
Anujmourya000 <Anujmourya000@gmail.com>
2021-04-27 20:19:59 +0530

    Added: UC3 comparison problem using compareTo method is added

--- LineComparisonProblem.java
+++ LineComparisonProblem.java
@@ -7,7 +7,7 @@ public class LineComparisonProblem {
 //	 As a fan of geometry, I want to model a line based on a point consisting of (x, y) co-ordinates
 //	 using the Cartesian system, So that I can calculate its length.
 	
-//		 A Length as 2 Points (x1, y1) and (x2, y2)
+//		 A Length as 4 Points (x1, y1), (x2, y2) , (x3, y3) , (x4, y4)
 		
 		int x1 = 2;
 		int x2 = 4;
@@ -15,20 +15,19 @@ public class LineComparisonProblem {
 		int y2 = 10;
 		
 		int x3 = 4;
-		int x4 = 6;
-		int y3 = 8;
-		int y4 = 12;
+		int x4 = 8;
+		int y3 = 12;
+		int y4 = 16;
 		
 		Integer line1 = (int) Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
 		
-		Integer line2 = (int) Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
-
-		if(line1.equals(line2))
-			
-			System.out.println("Lengths of both the lines are equal");
+		Integer line2 = (int) Math.sqrt((y4 - y3) * (y4 - y3) + (x4 - x3) * (x4 - x3));
 		
-		else
-			System.out.println("Not equal");
+			
+			System.out.println(line1.compareTo(line2));
+			System.out.println("Hence, line1 is less than line2");
+			
+		
 		
 	}
 	
